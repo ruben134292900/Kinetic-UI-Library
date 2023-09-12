5fb7e3d1381d3999af1e4245ba9be8ba4c791655
SlipperGamingHD <89786857+EbicSeal@users.noreply.github.com>
2023-09-12 23:01:47 +0200

    Delete Updater.lua

--- Updater.lua
+++
@@ -1,5 +0,0 @@
-local HTTP = game:GetService("HttpService")
-
-local Ver = loadstring(HTTP:GetAsync("https://raw.githubusercontent.com/EbicSeal/Kinetic-UI-Library/main/Version.lua"))
-	
-game.ReplicatedStorage.KineticUI.Latest.Value = Ver()
